ac654c7b1656094ddbba34be941ee20a731ce847
aien0501aura <kenhuang2019iii@gmail.com>
2019-10-26 15:02:51 +0800

    add style

---
+++ public/lib/popupWindow.js
@@ -0,0 +1,9 @@
+// chat box toggle
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
